19c4457a1c9a78664b4ad6fc2c19b5e46d15c82d
affanramadhan23 <affannabilramadhan@gmail.com>
2023-12-28 20:41:01 +0700

    Menambahkan OutlinedTextField No HP pada fun FormInputSiswa

--- app/src/main/java/com/example/pakeapi/ui/home/screen/InsertScreen.kt
+++ app/src/main/java/com/example/pakeapi/ui/home/screen/InsertScreen.kt
@@ -3,11 +3,13 @@ package com.example.pakeapi.ui.home.screen
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import com.example.pakeapi.navigation.DestinasiNavigasi
 import com.example.pakeapi.ui.home.viewmodel.InsertViewModel
@@ -47,6 +49,15 @@ fun FormInputSiswa(
             modifier = Modifier.fillMaxWidth(),
             enabled = enabled,
             singleLine = true
+        )
+        OutlinedTextField(
+            value = insertUiEvent.nohp,
+            onValueChange ={onValueChange(insertUiEvent.copy(nohp = it))},
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            label = { Text("No HP") },
+            singleLine = true,
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled
         )
     }
 }
