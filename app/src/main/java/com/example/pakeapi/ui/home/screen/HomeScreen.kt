7a07696f5c20a1b9ee63c9c67727972861d62aeb
affanramadhan23 <affannabilramadhan@gmail.com>
2023-12-28 20:09:13 +0700

    Mengganti fun HomeScreen menjadi HomeStatus,serta menambahkan param delete dan detail pada HomeStatus dan KontakLayout

--- app/src/main/java/com/example/pakeapi/ui/home/screen/HomeScreen.kt
+++ app/src/main/java/com/example/pakeapi/ui/home/screen/HomeScreen.kt
@@ -36,12 +36,21 @@ import com.example.pakeapi.ui.home.viewmodel.KontakUIState
 
 @Composable
 fun HomeScreen(
-    kontakUIState: KontakUIState, retryAction: () -> Unit, modifier: Modifier = Modifier
+    kontakUIState: KontakUIState, retryAction: () -> Unit, modifier: Modifier = Modifier,
+    onDeleteClick: (Kontak) -> Unit = {},
+    onDetailClick: (Kontak) -> Unit
 ){
     when (kontakUIState){
         is KontakUIState.Loading -> OnLoading(modifier = modifier.fillMaxSize())
         is KontakUIState.Success -> KontakLayout(
-            kontak = kontakUIState.kontak, modifier = modifier.fillMaxWidth()
+            kontak = kontakUIState.kontak,
+            modifier = modifier.fillMaxWidth(),
+            onDetailClick = {
+                onDetailClick(it)
+            },
+            onDeleteClick = {
+                onDeleteClick(it)
+            }
         )
         is KontakUIState.Error -> OnError(retryAction, modifier = modifier.fillMaxSize())
     }
@@ -77,7 +86,9 @@ fun OnError(retryAction: () -> Unit, modifier: Modifier = Modifier){
 @Composable
 fun KontakLayout(
     kontak: List<Kontak>,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    onDetailClick: (Kontak) -> Unit,
+    onDeleteClick: (Kontak) -> Unit = {}
 ){
     LazyColumn(
         modifier = Modifier,
@@ -87,7 +98,10 @@ fun KontakLayout(
         items(kontak){kontak ->
             KontakCard(kontak = kontak, modifier = Modifier
                 .fillMaxWidth()
-                .clickable { }
+                .clickable { onDetailClick(kontak)},
+                onDeleteClick = {
+                    onDeleteClick(kontak)
+                }
             )
         }
     }
