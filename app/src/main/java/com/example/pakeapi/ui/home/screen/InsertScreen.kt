4a1388822ad7d720628575125e9cfef8f5493ea0
affanramadhan23 <affannabilramadhan@gmail.com>
2023-12-28 20:33:47 +0700

    Menambahkan object DestinasiEntry

--- app/src/main/java/com/example/pakeapi/ui/home/screen/InsertScreen.kt
+++ app/src/main/java/com/example/pakeapi/ui/home/screen/InsertScreen.kt
@@ -1,2 +1,8 @@
 package com.example.pakeapi.ui.home.screen
 
+import com.example.pakeapi.navigation.DestinasiNavigasi
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = "Entry Siswa"
+}
