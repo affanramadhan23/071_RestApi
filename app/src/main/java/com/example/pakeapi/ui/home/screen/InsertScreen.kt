ca9a8b7b13bd32066aada59dd306eeeb4946e663
affanramadhan23 <affannabilramadhan@gmail.com>
2023-12-28 20:47:46 +0700

    Menambahkan Scaffold, TopAppBarKontak, EntryKontakBody pada EntryKontakScreen

--- app/src/main/java/com/example/pakeapi/ui/home/screen/InsertScreen.kt
+++ app/src/main/java/com/example/pakeapi/ui/home/screen/InsertScreen.kt
@@ -4,21 +4,29 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.pakeapi.navigation.DestinasiNavigasi
+import com.example.pakeapi.ui.TopAppBarKontak
 import com.example.pakeapi.ui.home.viewmodel.InsertViewModel
 import com.example.pakeapi.ui.home.viewmodel.PenyediaViewModel
+import kotlinx.coroutines.launch
 
 object DestinasiEntry : DestinasiNavigasi {
     override val route = "item_entry"
@@ -30,7 +38,35 @@ fun EntryKontakScreen(
     navigateBack: () -> Unit,
     modifier: Modifier = Modifier,
     viewModel: InsertViewModel = viewModel(factory = PenyediaViewModel.Factory)
-){}
+){
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            TopAppBarKontak(
+                title = DestinasiEntry.titleRes,
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior,
+                navigateUp = navigateBack
+            )
+        }
+    ) {innerPadding ->
+        EntryKontakBody(
+            insertUiState = viewModel.insertkontakState,
+            onSiswaValueChange = viewModel::updateInsertKontakState,
+            onSaveClick = { coroutineScope.launch {
+                viewModel.insertKontak()
+                navigateBack()
+            }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
+    }
+}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
