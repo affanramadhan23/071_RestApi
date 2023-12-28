13c09d25bea2eca921f522afce588c8f36fa656c
affanramadhan23 <affannabilramadhan@gmail.com>
2023-12-28 21:00:53 +0700

    Memanggil PengelolaHalaman pada KontakApp.

--- app/src/main/java/com/example/pakeapi/ui/KontakApp.kt
+++ app/src/main/java/com/example/pakeapi/ui/KontakApp.kt
@@ -20,6 +20,7 @@ import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.pakeapi.R
+import com.example.pakeapi.navigation.PengelolaHalaman
 import com.example.pakeapi.ui.home.viewmodel.HomeViewModel
 import com.example.pakeapi.ui.home.viewmodel.PenyediaViewModel
 
@@ -38,7 +39,7 @@ fun KontakApp(
                 .fillMaxSize()
                 .padding(it)
         ) {
-
+            PengelolaHalaman()
         }
     }
 }
