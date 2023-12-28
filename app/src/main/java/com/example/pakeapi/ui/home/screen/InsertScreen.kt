42b20e9bae376a5afe53b1b46a4137e6b6b530e9
affanramadhan23 <affannabilramadhan@gmail.com>
2023-12-28 20:45:48 +0700

    Menambahkan button simpan pada EntryKontakBody dan memperbaiki typo.

--- app/src/main/java/com/example/pakeapi/ui/home/screen/InsertScreen.kt
+++ app/src/main/java/com/example/pakeapi/ui/home/screen/InsertScreen.kt
@@ -5,8 +5,10 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -79,7 +81,7 @@ fun FormInputSiswa(
 fun EntryKontakBody(
     insertUiState: InsertViewModel.InsertUiState,
     onSiswaValueChange: (InsertViewModel.InsertUiEvent) -> Unit,
-    noSaveClick: () -> Unit,
+    onSaveClick: () -> Unit,
     modifier: Modifier = Modifier
 ){
     Column (
@@ -91,5 +93,12 @@ fun EntryKontakBody(
             onValueChange = onSiswaValueChange,
             modifier = Modifier.fillMaxWidth()
         )
+        Button(
+            onClick = onSaveClick,
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = "Simpan")
+        }
     }
 }
