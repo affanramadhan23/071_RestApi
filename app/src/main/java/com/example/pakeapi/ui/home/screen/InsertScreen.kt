1fd8ce6332ec5d5d8d9667321f94343389cb9f5b
affanramadhan23 <affannabilramadhan@gmail.com>
2023-12-28 20:34:39 +0700

    Membuat fun EntryKontakScreen

--- app/src/main/java/com/example/pakeapi/ui/home/screen/InsertScreen.kt
+++ app/src/main/java/com/example/pakeapi/ui/home/screen/InsertScreen.kt
@@ -1,8 +1,13 @@
 package com.example.pakeapi.ui.home.screen
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
 import com.example.pakeapi.navigation.DestinasiNavigasi
 
 object DestinasiEntry : DestinasiNavigasi {
     override val route = "item_entry"
     override val titleRes = "Entry Siswa"
 }
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EntryKontakScreen(){}
