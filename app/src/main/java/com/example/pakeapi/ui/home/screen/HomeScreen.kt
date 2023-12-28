8f0a30fb6f7127e8ade3b0e5c5e859c7c6eb4e6a
affanramadhan23 <affannabilramadhan@gmail.com>
2023-12-28 20:20:01 +0700

    Menambahkan fun HomeScreen serta memperbaiki fun HomeStatus

--- app/src/main/java/com/example/pakeapi/ui/home/screen/HomeScreen.kt
+++ app/src/main/java/com/example/pakeapi/ui/home/screen/HomeScreen.kt
@@ -14,32 +14,42 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Phone
 import androidx.compose.material3.AlertDialogDefaults.shape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.pakeapi.R
 import com.example.pakeapi.model.Kontak
 import com.example.pakeapi.navigation.DestinasiNavigasi
+import com.example.pakeapi.ui.PenyediaViewModel
+import com.example.pakeapi.ui.TopAppBarKontak
+import com.example.pakeapi.ui.home.viewmodel.HomeViewModel
 import com.example.pakeapi.ui.home.viewmodel.KontakUIState
 
 
 @Composable
-fun HomeScreen(
+fun HomeStatus(
     kontakUIState: KontakUIState, retryAction: () -> Unit, modifier: Modifier = Modifier,
     onDeleteClick: (Kontak) -> Unit = {},
-    onDetailClick: (Kontak) -> Unit
+    onDetailClick: (Int) -> Unit
 ){
     when (kontakUIState){
         is KontakUIState.Loading -> OnLoading(modifier = modifier.fillMaxSize())
@@ -47,7 +57,7 @@ fun HomeScreen(
             kontak = kontakUIState.kontak,
             modifier = modifier.fillMaxWidth(),
             onDetailClick = {
-                onDetailClick(it)
+                onDetailClick(it.id)
             },
             onDeleteClick = {
                 onDeleteClick(it)
@@ -153,7 +163,54 @@ fun KontakCard(
         override val route = "home"
         override val titleRes = "Kontak"
     }
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun HomeScreen(
+        navigateToItemEntry: () -> Unit,
+        modifier: Modifier = Modifier,
+        onDetailClick: (Int) -> Unit = {},
+        viewModel: HomeViewModel = viewModel(factory = PenyediaViewModel.Factory)
+    ) {
+        val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
 
+        Scaffold(
+            modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+            topBar = {
+                TopAppBarKontak(
+                    title = DestinasiHome.titleRes,
+                    canNavigateBack = false,
+                    scrollBehavior = scrollBehavior
+                ) {}
+            },
+            floatingActionButton = {
+                FloatingActionButton(
+                    onClick = navigateToItemEntry,
+                    shape = MaterialTheme.shapes.medium,
+                    modifier = Modifier.padding(18.dp)
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Add,
+                        contentDescription = "Add Kontak"
+                    )
+                }
+            }
+        ) { innerPadding ->
+
+            HomeStatus(
+                kontakUIState = viewModel.kontakUIState,
+                retryAction = {
+                    viewModel.getKontak()
+                },
+                modifier = Modifier.padding(innerPadding),
+
+                onDetailClick = onDetailClick,
+                onDeleteClick = {
+                    viewModel.deleteKontak(it.id)
+                    viewModel.getKontak()
+                }
+            )
+        }
+    }
 }
 
 
