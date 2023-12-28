a730e9d83a3fb67a492d8163ab3511a49905acfa
affanramadhan23 <affannabilramadhan@gmail.com>
2023-12-28 20:37:43 +0700

    Menambahkan param pada fun FormInputSiswa

--- app/src/main/java/com/example/pakeapi/ui/home/screen/InsertScreen.kt
+++ app/src/main/java/com/example/pakeapi/ui/home/screen/InsertScreen.kt
@@ -2,7 +2,9 @@ package com.example.pakeapi.ui.home.screen
 
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import com.example.pakeapi.navigation.DestinasiNavigasi
+import com.example.pakeapi.ui.home.viewmodel.InsertViewModel
 
 object DestinasiEntry : DestinasiNavigasi {
     override val route = "item_entry"
@@ -14,4 +16,9 @@ fun EntryKontakScreen(){}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun FormInputSiswa(){}
+fun FormInputSiswa(
+    insertUiEvent: InsertViewModel.InsertUiEvent,
+    modifier: Modifier = Modifier,
+    onValueChange : (InsertViewModel.InsertUiEvent) -> Unit = {},
+    enabled: Boolean = true
+){}
